d95ced6f15c02814b0d2dc2ab395084138c3734a
Hemeema Gunthoti <111888127+Hemeema-gunthoti@users.noreply.github.com>
2026-07-04 14:19:29 +0530

    model change

--- log-triager/agents/sdet_2_classify.py
+++ log-triager/agents/sdet_2_classify.py
@@ -55,7 +55,7 @@ def classify_logs(log_entries, api_key):
     system_prompt, user_prompt = build_classification_prompt(log_entries)
     
     message = client.chat.completions.create(
-        model="mixtral-8x7b-32768",
+        model="llama-3.3-70b-versatile",
         max_tokens=2000,
         messages=[
             {"role": "system", "content": system_prompt},
